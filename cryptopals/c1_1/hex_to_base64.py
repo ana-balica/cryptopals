07e1cc76601ddbad101be5fc067d7ae454936f65
ana-balica <ana.balica@gmail.com>
2017-04-08 10:40:47 +0100

    Move things around and make local installable

--- cryptopals/c1_1/hex_to_base64.py
+++ cryptopals/c1_1/hex_to_base64.py
@@ -16,20 +16,6 @@ def hex_to_base64(hex_string):
     return b2a_base64(unhexlify(hex_string)).rstrip()
 
 
-def xor_hex(hex1, hex2):
-    """
-    Compute XOR between two equal-length buffers in hex.
-    Result is also a hex represented as a string.
-
-    Challenge 1.2: https://cryptopals.com/sets/1/challenges/2
-
-    >>> xor_hex("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965")
-    '746865206b696420646f6e277420706c6179'
-    """
-    xor_result = int(hex1, 16) ^ int(hex2, 16)
-    return '{0:x}'.format(xor_result)
-
-
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(
+    name='Cryptopals solutions by Ana',
+    version='0.1dev',
+    packages=['cryptopals',],
+)
