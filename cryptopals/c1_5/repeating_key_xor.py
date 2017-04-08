ef7c322091ab7770c3ae8f4d70ee599bc4b2bf26
ana-balica <ana.balica@gmail.com>
2017-04-08 15:13:05 +0100

    Fix converting from str to hex

--- cryptopals/c1_5/repeating_key_xor.py
+++ cryptopals/c1_5/repeating_key_xor.py
@@ -13,14 +13,14 @@ def xor_encrypt(message, key):
     Challenge 1.5: https://cryptopals.com/sets/1/challenges/5
 
     >>> xor_encrypt("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal", "ICE")
-    '0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f'
+    b'0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f'
     """
     encrypted_letters = []
     for letter, key_char in zip(message, itertools.cycle(key)):
         xor_char = xor(letter, key_char)
         encrypted_letters.append(xor_char)
 
-    return hexlify(''.join(encrypted_letters))
+    return hexlify(''.join(encrypted_letters).encode('utf-8'))
 
 
 if __name__ == '__main__':
