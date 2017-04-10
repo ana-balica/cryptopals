01ed7a767df15fdcaa87d380b78e8b7244fce408
ana-balica <ana.balica@gmail.com>
2017-04-10 21:46:40 +0100

    Allow strings or bytes to be passed to hamming distance func

--- cryptopals/c1_6/break_repeating_key_xor.py
+++ cryptopals/c1_6/break_repeating_key_xor.py
@@ -2,14 +2,20 @@ def get_hamming_distance(left, right):
     """
     Calculate hamming distance - the number of different bits.
 
+    >>> get_hamming_distance(b'this is a test', b'wokka wokka!!!')
+    37
+
     >>> get_hamming_distance('this is a test', 'wokka wokka!!!')
     37
     """
-    left_bytes = left.encode('utf-8')
-    right_bytes = right.encode('utf-8')
+    if isinstance(left, str):
+        left = left.encode('utf-8')
+
+    if isinstance(right, str):
+        right = right.encode('utf-8')
 
     hd = 0
-    for left_byte, right_byte in zip(left_bytes, right_bytes):
+    for left_byte, right_byte in zip(left, right):
         left_bits = to_bits(left_byte)
         right_bits = to_bits(right_byte)
         hd += sum(x != y for x, y in zip(left_bits, right_bits))
